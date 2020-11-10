dbcfcf592474a6fbc6c9dbaf3df32c176df6deb6
Guy Khmelnitsky <3136012+GuyKh@users.noreply.github.com>
2020-11-10 11:22:42 +0200

    Update config_flow.py

--- custom_components/electrasmart/config_flow.py
+++ custom_components/electrasmart/config_flow.py
@@ -1,13 +1,61 @@
 """Adds config flow for electrasmartclimatesensor."""
 from collections import OrderedDict
 import logging
+import re
 import voluptuous as vol
 
 from homeassistant import config_entries
 
-from .const import DOMAIN
+from .const import { DOMAIN }
+
+from electrasmart import { send_otp_request, get_otp_token, get_devices, AC, ElectraAPI }
 
 _LOGGER = logging.getLogger(__name__)
+
+
+async def is_phone_valid(
+    data,
+):
+    """Validate the user input allows us to connect.
+    Data has the keys from DATA_SCHEMA with values provided by the user.
+    Request configuration steps from the user.
+    """
+
+    phone = data.get("phone")
+    if phone is not None:
+        phonePattern = "[0-9]{10}"
+        m = re.match(phonePattern, phone)
+
+        if m:
+            return {
+                "title": "phone",
+                "phone": data.get("phone")
+            }
+        else:
+            return None
+    return None
+
+async def is_otp_valid(
+    data,
+):
+    """Validate the user input allows us to connect.
+    Data has the keys from DATA_SCHEMA with values provided by the user.
+    Request configuration steps from the user.
+    """
+
+    otp = data.get("otp")
+    if otp is not None:
+        otpPattern = "[0-9]{4}"
+        m = re.match(otpPattern, otp)
+
+        if m:
+            return {
+                "title": "otp",
+                "otp": data.get("otp")
+            }
+        else:
+            return None
+    return None
 
 
 @config_entries.HANDLERS.register(DOMAIN)
@@ -58,20 +106,81 @@ class ElectraSmartClimateSensorFlowHandler(config_entries.ConfigFlow, domain=DOM
             if "imei" in user_input:
                 device_class = user_input["imei"]
 
-        data_schema = OrderedDict()
-        data_schema[vol.Required("name", default=name)] = str
-        data_schema[vol.Required("template", default=template)] = str
-        data_schema[vol.Optional("device_class", default=device_class)] = str
+            return self.async_create_entry(title=info["title"], data=info["data"])
+
+
         return self.async_show_form(
-            step_id="user", data_schema=vol.Schema(data_schema), errors=self._errors
+            step_id="user", data_schema=self._async_build_schema(), errors=errors
         )
 
-    async def _validate_template(self, template):
-        """Return true if template is valid."""
-        try:
-            templater.Template(template, self.hass).async_render()
-            return True
-        except Exception as exception:  # pylint: disable=broad-except
-            _LOGGER.error(exception)
-            pass
-        return False
+
+    async def async_get_phone(self, user_input=None):
+        errors = {}
+        if user_input is not None:
+            # Validate user input
+            valid = await is_phone_valid(user_input)
+            if valid:
+                # Store phone to use in next step
+                self.phone = user_input["phone"]
+                self.imei = send_otp_request(self.phone)
+
+                # Return the form of the next step
+                return await self.async_get_ota_token()
+            
+            errors["base"] = "invalid_phone"
+
+        # Specify items in the order they are to be displayed in the UI
+        data_schema = {
+            vol.Required("phone"): str,
+        }
+
+        return self.async_show_form(
+            step_id="get_phone", data_schema=vol.Schema(data_schema), errors=errors
+        )
+
+    async def async_get_ota_token(self, user_input=None):
+        errors = {}
+        if user_input is not None:
+            # Validate user input
+            valid = await is_otp_valid(user_input)
+            if valid:
+                # Store info to use in next step
+                otp = user_input["otp"]
+                self.token = get_otp_token(self.imei, self.phone, otp)
+
+                # Return the form of the next step
+                return await self.async_get_devices()
+
+            errors["base"] = "invalid_token"
+
+        # Specify items in the order they are to be displayed in the UI
+        data_schema = {
+            vol.Required("token"): str,
+        }
+
+        return self.async_show_form(
+            step_id="get_phone", data_schema=vol.Schema(data_schema), errors=errors
+        )
+
+    async def get_devices(self):
+        errors = {}
+
+        devices = get_devices(self.imei, self.token)
+
+        acs = []
+        
+        for device in devices:
+            device_id = device['id']
+            device_name = device['name']
+            ac = { "id": device_id, "name": device_name }
+            acs.append(ac)            
+
+        self.acs = acs
+
+        return self.async_create_entry(
+            title="ElectraSmart",
+            data={
+                "imei": self.imei,
+                "token": self.token,
+                "acs": self.acs
+            })
